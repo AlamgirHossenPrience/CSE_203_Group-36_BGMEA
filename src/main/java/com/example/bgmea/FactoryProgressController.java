1ce4c5ab7200c554a92a2824c595617b99f1474a
Mahathir003 <mahathirmahi03@gmail.com>
2025-04-14 19:05:29 +0600

    worked on fxml

---
+++ src/main/java/com/example/bgmea/FactoryProgressController.java
@@ -0,0 +1,32 @@
+package com.example.bgmea;
+
+import javafx.scene.input.MouseEvent;
+
+public class FactoryProgressController
+{
+    @javafx.fxml.FXML
+    private TableColumn factoryNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn exportedYearlyColumn;
+    @javafx.fxml.FXML
+    private TableColumn productionRateColumn;
+    @javafx.fxml.FXML
+    private TextField searchFactoryTextField;
+    @javafx.fxml.FXML
+    private TableColumn exportedQuarterlyColumn;
+    @javafx.fxml.FXML
+    private TableView allFactoryProgressTable;
+    @javafx.fxml.FXML
+    private TableColumn exportedMonthlyColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void searchFactoryOnMouseClicked(Event event) {
+    }
+
+    public void searchFactoryOnMouseClicked(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/main/java/com/example/bgmea/PresidentDashboardController.java
@@ -0,0 +1,39 @@
+package com.example.bgmea;
+
+import javafx.event.Event;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class PresidentDashboardController
+{
+    @javafx.fxml.FXML
+    private TableColumn pendingApprovalColumn;
+    @javafx.fxml.FXML
+    private TableView pendingApprovalTable;
+    @javafx.fxml.FXML
+    private TableColumn serialColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void refreshPendingApprovalsButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void activeStatusButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void makeAnAnnouncementButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void pendingMemberRequestButton(Event event) {
+    }
+
+    @javafx.fxml.FXML
+    public void factroryProgressButton(Event event) {
+    }
+}
