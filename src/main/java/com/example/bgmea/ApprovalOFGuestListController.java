cc8f6990675ac79fa500a0bdb3f00a5de14b2e91
Mahathir003 <mahathirmahi03@gmail.com>
2025-04-15 20:57:59 +0600

    newly added fxml

---
+++ src/main/java/com/example/bgmea/ApprovalOFGuestListController.java
@@ -0,0 +1,7 @@
+package com.example.bgmea;
+
+public class ApprovalOFGuestListController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/example/bgmea/ApprovalOfEventBudgetController.java
@@ -0,0 +1,20 @@
+package com.example.bgmea;
+
+public class ApprovalOfEventBudgetController
+{
+    @javafx.fxml.FXML
+    private TableView eventBudgetListTable;
+    @javafx.fxml.FXML
+    private Label eventNameBudgetListLabel;
+    @javafx.fxml.FXML
+    private TableColumn projectedSubTotalColumn;
+    @javafx.fxml.FXML
+    private Label totalBudgetLabel;
+    @javafx.fxml.FXML
+    private TableColumn categoryColumn;
+    @javafx.fxml.FXML
+    private Button approveEventBudgetButton;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
