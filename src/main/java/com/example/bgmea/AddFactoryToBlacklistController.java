e8929454f9e3a1a9b7b0dee34a924ac4f466eb66
AlamgirHossenPrience <alamgir.prience@gmail.com>
2025-04-16 22:05:47 +0600

    Add FXML

---
+++ src/main/java/com/example/bgmea/AddFactoryToBlacklistController.java
@@ -0,0 +1,37 @@
+package com.example.bgmea;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class AddFactoryToBlacklistController
+{
+    @javafx.fxml.FXML
+    private TextField panaltyAmountTextField;
+    @javafx.fxml.FXML
+    private ComboBox violationCombobox;
+    @javafx.fxml.FXML
+    private TextArea reasonForBlacklistTextArea;
+    @javafx.fxml.FXML
+    private DatePicker blacklistedDateDatePicker;
+    @javafx.fxml.FXML
+    private TextField blacklistedByTextField;
+    @javafx.fxml.FXML
+    private TextArea outputTextArea;
+    @javafx.fxml.FXML
+    private ComboBox factoryCombobox;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void LCODashboardButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addToBlacklistButtonOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/bgmea/RemoveFactoryFromBlacklistController.java
@@ -0,0 +1,41 @@
+package com.example.bgmea;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
+public class RemoveFactoryFromBlacklistController
+{
+    @javafx.fxml.FXML
+    private TableColumn nameTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn violationTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn codeTableColumn;
+    @javafx.fxml.FXML
+    private TextArea outputTextArea;
+    @javafx.fxml.FXML
+    private ComboBox blacklistedFactoryTextField;
+    @javafx.fxml.FXML
+    private TableView blaclistedFactoryTableview;
+    @javafx.fxml.FXML
+    private TableColumn dateTableColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void showBlacklistedFectoryButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LCODashboardButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmButtonOnAction(ActionEvent actionEvent) {
+    }
+}
