bb5c7743195d3b550ebbdae7d350b7064a61f0ec
AlamgirHossenPrience <alamgir.prience@gmail.com>
2025-04-17 01:50:15 +0600

    Add FXML

---
+++ src/main/java/com/example/bgmea/TrackTrainingCompletionStatusController.java
@@ -0,0 +1,27 @@
+package com.example.bgmea;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
+public class TrackTrainingCompletionStatusController
+{
+    @javafx.fxml.FXML
+    private ComboBox trainingProgramsCombobox;
+    @javafx.fxml.FXML
+    private TextArea outputTextArea;
+    @javafx.fxml.FXML
+    private TextArea reviewTextArea;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void TECDashboardButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void markedAsCompletedButtonOnAction(ActionEvent actionEvent) {
+    }
+}
