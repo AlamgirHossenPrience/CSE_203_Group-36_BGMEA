4c819ff8eab2029a4dbe5540046a223f521c1547
AlamgirHossenPrience <alamgir.prience@gmail.com>
2025-04-16 18:54:33 +0600

    add fxml

---
+++ src/main/java/com/example/bgmea/CreateAnnouncementsController.java
@@ -0,0 +1,24 @@
+package com.example.bgmea;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class CreateAnnouncementsController
+{
+    @javafx.fxml.FXML
+    private TextArea outputTextArea;
+    @javafx.fxml.FXML
+    private TextArea messageTextArea;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void LCODashboardButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void creatAnnouncementButtonOnAction(ActionEvent actionEvent) {
+    }
+}
