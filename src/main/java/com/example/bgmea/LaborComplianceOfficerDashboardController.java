764815bbbabb2349f00ee42888a9546ab2c30361
AlamgirHossenPrience <alamgir.prience@gmail.com>
2025-04-19 08:28:51 +0600

    Add FXML

---
+++ src/main/java/com/example/bgmea/LaborComplianceOfficerDashboardController.java
@@ -0,0 +1,46 @@
+package com.example.bgmea;
+
+import javafx.event.ActionEvent;
+
+public class LaborComplianceOfficerDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void conductFactorySafetyAuditButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void issueComplianceViolationNoticeButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addFactoryToBlacklistButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageAnnouncementsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageComplianceReportsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void signOutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void complianceTecordingOptionButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeFactoryFromBlacklistButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void registerNewFactoryButtonOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/bgmea/TrainingandEducationCoordinatorDashboardController.java
@@ -0,0 +1,46 @@
+package com.example.bgmea;
+
+import javafx.event.ActionEvent;
+
+public class TrainingandEducationCoordinatorDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void AssignTrainertoProgramButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void EnrollWorkersinTrainingButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RemoveWorkersFromTrainingButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ModifyTrainingProgramButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DeleteTrainingProgramButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CreateNewTrainingProgramButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void TrackTrainingCompletionStatusButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ScheduleTrainingSessionButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void signOutButtonOnAction(ActionEvent actionEvent) {
+    }
+}
