5147021e7d7af96d996abbef3c0a0ead1ab5eec9
Mahathir003 <mahathirmahi03@gmail.com>
2025-04-21 22:58:20 +0600

    fixation

--- src/main/java/com/example/bgmea/HelloApplication.java
+++ src/main/java/com/example/bgmea/HelloApplication.java
@@ -1,17 +1,18 @@
-package com.example.bgmea;
+package com.example.calculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class HelloApplication extends Application {
+import java.io.IOException;
 
+public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/bgmea/MahathirFXML/PresidentDashboard.fxml"));
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(""));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("President Dashboard");
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
